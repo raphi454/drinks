f0b43269a29f150d3e4b0e503fe1b406687a5363
rsteger <rsteger@schueler.hakzell.at>
2025-12-01 13:02:11 +0100

    add milk

--- src/RandomDrink.php
+++ src/RandomDrink.php
@@ -4,7 +4,7 @@ namespace Raphi454\Drinks;
 
 class RandomDrink
 {
-private array $drink = ['Wasser', 'Saft'];
+private array $drink = ['Wasser', 'Saft', 'Milch'];
 
 public function getDrink(): string{
     return $this->drink[array_rand($this->drink)];
